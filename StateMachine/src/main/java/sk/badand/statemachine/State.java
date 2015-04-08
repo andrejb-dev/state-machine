f9964df40c7977062d92cf021333e1e391fea827
Zavael <cccp14@atlas.sk>
2015-04-08 16:08:09 +0200

    commits

--- StateMachine/src/main/java/sk/badand/statemachine/State.java
+++ StateMachine/src/main/java/sk/badand/statemachine/State.java
@@ -9,7 +9,22 @@ package sk.badand.statemachine;
  */
 public interface State {
     
+    // TODO: introduce Default EndState instead of null
+    // TODO: introduce AbstractState that forces to call changeState?
+    
+    /**
+     * Is called at entering this state to machine's current state
+     */
     public void enterState(Machine machine);
+    
+    /**
+     * Is called as main execution of state logic.
+     * At the end of the implementation it should call machine.changeToState(nextState) or machine.stopMachine
+     */
     public void executeState(Machine machine);
+    
+    /**
+     * Is called at exiting from this state
+     */
     public void exitState(Machine machine);
 }
